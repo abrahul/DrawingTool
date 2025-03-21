3caafc5fce4faace09e1b12b85880e81ab2ae7fd
abrahul100 <abrahul100@gmail.com>
2025-03-21 13:53:57 +0530

    Added canvas for drawing

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Simple Drawing Tool")
 
+#Added a drawing canvas
+canvas = tk.Canvas(root, bg="white", width=800, height=600)
+canvas.pack()
+
 # Run the Tkinter event loop
 root.mainloop()
